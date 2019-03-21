69ea71d7333b6d9c38b1cf923fcedc3c5931edae
Kisházi János <janoskishazi@gmail.com>
2019-03-21 21:41:18 +0100

    Added new exercise, strings.

---
+++ 008-Strings.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+## General info:
+##	* The created program have to communicate in english with the user.
+##	  Furthermore the code variables/methods/classes etc... be named in English.
+##
+##	* The program have to possible to handle the wrong user inputs and warnings the user how to use the program.
+##
+##	* When you use 3rd party code from stackoverflow or other source, always reference this in the code in a comment.
+##	  Even if you modified the code.
+##
+## Task: 008-Strings:
+## This exercise is based on the string operations in Python.
+## Solve every task of the exercise and print the resut to the standard output.
+##
+## @task-author Kishazi "janohhank" Janos
+## @implementation-author
+
+message="Hello, this is a Python program."
+
+# 1. task
+# Add a hashtag ('#') between every character of the message.
+# The first character is a H and the last karacter is the '.'.
+
+# 2. task
+# Change every 'L' character to '1', ecery 'e' to '3' and every a to '4'.
+
+# 3. task
+# Split the message into a list of string by the spaces.
+
+# 4. task
+# Change 'this' and 'Python' words to upper case in the message.
+
+# 5. task
+# Save every words of the message into different files to the disk.
+
+# 6. task
+# Add your name after the 'Hello' in the message.
+
+# 7. task
+# Write the message reverse.
