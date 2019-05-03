be5faa23e43ee7d06cd58c5459ebf13593d6c671
Kisházi János <janohhank@xenon>
2019-05-03 08:05:08 +0200

    Added new exercise.

---
+++ 013-ATM.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+## General info:
+##	* The created program have to communicate in english with the user.
+##	  Furthermore the code variables/methods/classes etc... be named in English.
+##
+##	* The program have to possible to handle the wrong user inputs and warnings the user how to use the program.
+##
+##	* When you use 3rd party code from stackoverflow or other source, always reference this in the code in a comment.
+##	  Even if you modified the code.
+##
+## Task: 013-ATM
+## Write an algrotihm which can calculate the change of a an ATM transaction.
+## The program gets a number from the standard input and the output is the least return banknote and cash.
+## The currency is hungarian forint.
+##
+## For example 2245 = 2000 + 200 + 2x20 + 5
+##
+## @task-author Kishazi "janohhank" Janos
+## @implementation-author
