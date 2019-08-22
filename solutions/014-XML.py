ab4eff16c686b45e4c61cebdf9fb27477dfc14e1
Kisházi janohhank János <janoskishazi@gmail.com>
2019-08-22 11:14:19 +0100

    Finished 014-XML task solution.

--- solutions/014-XML.py
+++ solutions/014-XML.py
@@ -33,11 +33,16 @@ import argparse
 # Implements a simple and efficient API for parsing and creating XML data.
 import xml.etree.ElementTree
 
+# Global varibale
+gbpToHuf = float(358)
+
 parser = argparse.ArgumentParser(description="014-XML.")
 parser.add_argument("--inputXMLFilePath",required=True,type=str,help="The input XML file path.")
+parser.add_argument("--modifiedXMLFilePath",required=True,type=str,help="The output XML file path.")
 
 args = parser.parse_args()
 inputXMLFilePath = args.inputXMLFilePath
+modifiedXMLFilePath = args.modifiedXMLFilePath
 
 if(os.path.isfile(inputXMLFilePath) is False):
 	raise Exception("The input path does not denote a file: " + inputXMLFilePath)
@@ -69,3 +74,24 @@ for book in root.findall("book"):
 	if(genre == "Science Fiction"):
 		scienceFictionBooks.append(titleName)
 print("[" + __file__ + "]" + "[INFO]" + " The science fiction books are: " + str(scienceFictionBooks))
+
+# Inserting a new book.
+newBook = xml.etree.ElementTree.Element("book")
+author = xml.etree.ElementTree.SubElement(newBook,"author")
+author.text = "Isaac Asimov"
+title = xml.etree.ElementTree.SubElement(newBook,"title")
+title.text = "Foundation"
+genre = xml.etree.ElementTree.SubElement(newBook,"genre")
+genre.text = "Science Fiction"
+price = xml.etree.ElementTree.SubElement(newBook,"price")
+price.text = "10"
+root.insert(1,newBook)
+print("[" + __file__ + "]" + "[INFO]" + " New book was inserted: Isaac Asimov - Foundation.")
+
+# Modify books prices.
+for book in root.findall("book"):
+	currentPriceGBP = float(book.find("price").text)
+	currentPriceHUF = currentPriceGBP * gbpToHuf
+	book.find("price").text = str(currentPriceHUF)
+tree.write(modifiedXMLFilePath)
+print("[" + __file__ + "]" + "[INFO]" + " The GBP prices are modified and saved into the: " + str(modifiedXMLFilePath))
